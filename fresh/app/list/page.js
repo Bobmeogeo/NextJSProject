9fe50e3c2e7089e228cc21b407179f09ea92a22a
bobmeogeo <phgyn1234@gmail.com>
2023-08-26 16:07:20 +0900

    - List Page

---
+++ fresh/app/list/page.js
@@ -0,0 +1,20 @@
+export default function List(){
+
+      return(
+        <div>
+          <h4 className="title">상품목록</h4>
+          <div className="food">
+            <h4>상품1 $40</h4>
+          </div>
+          <div className="food">
+            <h4>상품2 $40</h4>
+          </div>
+        </div>
+      )
+    }
+
+    /* Next.js에서 URL과 페이지 만들기
+    1. app 폴더 안에 폴더만들기
+    2. page.js 넣기
+    3. 레이아웃 넣기
+    */
