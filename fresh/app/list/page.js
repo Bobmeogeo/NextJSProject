0b32515cbb3b915c985a193e6ff0b06da3833d48
jhoh <phgyn1234@gmail.com>
2023-08-27 12:37:34 +0900

    - List 페이지에 Map(a, i) 적용

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,14 +1,20 @@
 export default function List(){
 
+  let thing = ['Tomatoes', 'Pasta', 'Apple']
+
+
       return(
         <div>
           <h4 className="title">상품목록</h4>
-          <div className="food">
-            <h4>상품1 $40</h4>
-          </div>
-          <div className="food">
-            <h4>상품2 $40</h4>
-          </div>
+          {
+            thing.map((a, i)=>{
+              return(
+                <div className="food">
+                  <h4>{thing[i]} $40</h4>
+                </div>
+              )
+            })
+          }
         </div>
       )
     }
@@ -17,4 +23,11 @@ export default function List(){
     1. app 폴더 안에 폴더만들기
     2. page.js 넣기
     3. 레이아웃 넣기
+    */
+
+    /* html 내에서 for 반복문 사용 금지 -> map(a, i) 
+    - 함수안의 코드 반복 실행
+    - array안의 자료 출력줌
+    - i는 반복될 때마다 0부터 1씩 커짐
+    - return에 적은 것을 array로 담아준다.
     */
