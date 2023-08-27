4ed812a2b4fdb14769802e2cd9d7be139c9bc7e9
jhoh <phgyn1234@gmail.com>
2023-08-27 13:23:55 +0900

    - Cart Page 컴포넌트화 - data.js에서 데이터 import

---
+++ fresh/app/cart/data.js
@@ -0,0 +1,8 @@
+let string = '안녕하세요';
+
+export default string;
+
+/* export, import 
+1. 컴포넌트를 작성하고 export default 변수/함수; 작성
+2. 컴포넌트를 불러오기 import 이름 from "위치";
+*/
--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,36 @@
+import string from "./data"
+
 export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <p>{string}</p>
       </div>
     )
   } 
+
+  function CartItem(){
+    return(
+    <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+    </div>    
+    )
+  }
+
+  /* Component 만드는 법 
+  1. function 작명(){}
+  2. return(축약할 HTML)
+  3. <작명 /> 넣기
+  */
+
+  /* 서버, 클라이언트 컴포넌트 
+  1. server component: 아무데나 만들면 됨
+  - html에 자바스크립트 기능 넣기 불가능, 로딩속도 빠름, 큰페이지
+  2. client component: 파일 위에 'use client' 쓰기
+  - html에 자바스크립트 기능 넣기 가능, useState, useEffect 사용가능
+  */
